f3ad8e15317d0cafb04105d482a2242af1344841
Manasvi-1612 <123289723+Manasvi-1612@users.noreply.github.com>
2023-03-11 15:58:52 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,140 @@
+package YetAnotherQueryProblem;
+
+//{ Driver Code Starts
+    import java.io.*;
+    import java.util.*;
+    
+    
+    class IntArray
+    {
+        public static int[] input(BufferedReader br, int n) throws IOException
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+    
+            return a;
+        }
+    
+        public static void print(int[] a)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    
+        public static void print(ArrayList<Integer> a)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+    
+    
+    class IntMatrix
+    {
+        public static int[][] input(BufferedReader br, int n, int m) throws IOException
+        {
+            int[][] mat = new int[n][];
+    
+            for(int i = 0; i < n; i++)
+            {
+                String[] s = br.readLine().trim().split(" ");
+                mat[i] = new int[s.length];
+                for(int j = 0; j < s.length; j++)
+                    mat[i][j] = Integer.parseInt(s[j]);
+            }
+    
+            return mat;
+        }
+    
+        public static void print(int[][] m)
+        {
+            for(var a : m)
+            {
+                for(int e : a)
+                    System.out.print(e + " ");
+                System.out.println();
+            }
+        }
+    
+        public static void print(ArrayList<ArrayList<Integer>> m)
+        {
+            for(var a : m)
+            {
+                for(int e : a)
+                    System.out.print(e + " ");
+                System.out.println();
+            }
+        }
+    }
+    
+    class GFG {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int t;
+            t = Integer.parseInt(br.readLine());
+            while(t-- > 0){
+                
+                int N;
+                N = Integer.parseInt(br.readLine());
+                
+                
+                int num;
+                num = Integer.parseInt(br.readLine());
+                
+                
+                int[] A = IntArray.input(br, N);
+                
+                
+                int[][] Q = IntMatrix.input(br, num, 3);
+                
+                Solution obj = new Solution();
+                ArrayList<Integer> res = obj.solveQueries(N, num, A, Q);
+                
+                IntArray.print(res);
+                
+            }
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    class Solution {
+        public static ArrayList<Integer> solveQueries(int N, int num, int[] A, int[][] Q) {
+            HashMap<Integer,Integer> h=new HashMap<>();
+            int[] freq=new int[N];
+            ArrayList<Integer> arr=new ArrayList<>();
+            for(int i=N-1;i>=0;i--){
+                if(!h.containsKey(A[i])){
+                    h.put(A[i],1);
+                    freq[i]=h.get(A[i]);
+                }
+                else{
+                    freq[i]=h.get(A[i])+1;
+                    h.put(A[i],h.get(A[i])+1);
+                }
+            }
+            
+            for(int[] q : Q){
+                int l=q[0];
+                int r=q[1];
+                int k=q[2];
+                int c=0;
+                for(int i=l;i<=r;i++){
+                    if(freq[i]==k){
+                        c++;
+                    }
+                }
+                arr.add(c);
+            }
+            
+            
+            return arr;
+        }
+    }
+            
+    
